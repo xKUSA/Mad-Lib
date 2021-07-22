3063a39c93e1d24c427f84eee52047e0e2875590
Erkan <85198094+xKUSA@users.noreply.github.com>
2021-07-22 17:42:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mad_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralnoun, celebrity;
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+            Console.Write("Enter a plural noun: ");
+            pluralnoun = Console.ReadLine();
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralnoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+
+            
+        }
+    }
+}
